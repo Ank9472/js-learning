9ca687497a05462fddd752e8c522f77363dd3dc6
Ankit Tiwari <ank947214@gmail.com>
2024-07-15 17:55:28 +0000

    my file

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,6 +1,12 @@
 const name = "hitesh"
 const repoCount = 50
 
-console.log(name + repoCount + "Value");
+// console.log(name + repoCount + "Value");
+const gamename = String('ankitank')
 
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
 
