8d02d85102f3495bee4caac2d4306fdec3508c4e
Ankit Tiwari <ank947214@gmail.com>
2024-07-16 16:16:57 +0000

    add file

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,47 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.tofixed(1));
+
+const othernumber = 123.8966
+
+// console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++ maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)
+// );
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.min(4,5,6,8));
+
+
+console.log(Math.max(6,8,53));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,12 +1,41 @@
-const name = "hitesh"
+const name = "hitesh-ank"
 const repoCount = 50
 
 // console.log(name + repoCount + "Value");
 const gamename = String('ankitank')
 
-console.log(gamename[0]);
-console.log(gamename.__proto__);
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
 
-console.log(gamename.length);
-console.log(gamename.toUpperCase());
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+
+console.log(gamename.
+charAt(2))
+console.log(gamename.indexOf('t'));
+
+const newstring = gamename.substring(0,4)
+console.log(newstring);
+
+const anotherstring = gamename.slice(-8,4)
+console.log(anotherstring);
+
+const newstringone = " ankit  "
+console.log(newstringone);
+console.log(newstringone.trim())
+
+const URL = ("htttps:/ank.com/ank%26tiwari")
+
+console.log(URL.replace('%26','*'))
+
+
+
+
+
+
+
+
+
+
+
 
