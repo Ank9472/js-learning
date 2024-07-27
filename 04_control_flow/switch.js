58f8cf4a26b11c5b279db7aa7c8e86b7c7fa2977
Ankit Tiwari <ank947214@gmail.com>
2024-07-27 21:46:31 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,70 @@
+// 
+const month = 3
+
+switch(month){
+  
+    case 1:
+      console.log("january");
+
+        break;
+        default:
+
+    case 2:
+      console.log("febraury");
+
+      break;
+      
+    case 3:
+      console.log("march");
+
+        break;
+       
+    case 4:
+      console.log("april");
+        
+      break;
+      
+    case 5:
+      console.log("May");
+
+        break;
+       
+
+    case 6:
+      console.log("June");
+
+      break;
+     
+
+    case 7:
+      console.log("July");
+      break;
+     
+
+    case 8:
+      console.log("August");
+      break;
+      
+    case 9:
+      console.log("September");    
+      break;
+  
+    case 10:
+      console.log("October");
+      break;
+   
+    case 11:
+      console.log("November");
+      break;
+
+    case 12:
+      console.log("december");
+    
+      break;
+
+    
+console.log("default case match")
+break;
+
+
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail =[]
+
+if (userEmail) {
+  console.log("Got user email");
+}else {
+    console.log("Don't have user email");}
+ // yaha par single loop ke andar sara value nhi de skte 
+ 
+ //falsy values- false , 0, -0 , BigInt 0n,"",null,undefined,NaN
+
+//{}-object
+
+ //truthy value -true,[],"0",'false',"space",{},
