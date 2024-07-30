161915a3090d1881ff9005fd6c5afbf6aaefaba7
Ankit Tiwari <ank947214@gmail.com>
2024-07-30 17:58:32 +0000

    break and continue

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,13 +1,44 @@
-const userEmail =[]
+const userEmail =""
 
-if (userEmail) {
-  console.log("Got user email");
-}else {
-    console.log("Don't have user email");}
+// if (userEmail) {
+//   console.log("Got user email");
+//  }else {
+//    console.log("Don't have user email");}
+
+
  // yaha par single loop ke andar sara value nhi de skte 
  
  //falsy values- false , 0, -0 , BigInt 0n,"",null,undefined,NaN
 
 //{}-object
 
- //truthy value -true,[],"0",'false',"space",{},
+ //truthy value -true,[],"0",'false',"space",{},funcution(){}
+
+//  if(userEmail.length === 0){
+// console.log("Array is empty");
+//  }
+
+
+// if  (Object.keys(emptyobj).length===0) {
+//   console.log("object is empty");
+// }
+
+// nullish coalescing operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 12
+// val1 = null  ?? 25
+//val1 = undefined ?? 25
+
+// console.log(val1);
+
+//terniary operation 
+
+ //condition ? true : false
+
+const iceteaprice = 56
+iceteaprice >= 80 ? console.log("more than 80") : console.log("less than 80")
+
+
+
+
---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+// for
+
+for( let i = 0; i < 15; i ++) {
+   //const element = i;
+// console.log(element);   
+}
+
+
+for (let i= 0; i< 10; i++) {
+ // console.log('outer loop value: ${i}')    
+for (let j= 0; j < 10; j++) {
+
+// console.log('inner loop value ${j} and inner loop ${i}');
+}
+    
+}
+
+
+// break and continue 
+
+
+for (let index = 0; index < 15; index++) {
+
+    
+console.log('value of i is ${index}');
+
+}
