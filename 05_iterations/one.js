e4b29d88d100debf541bb75ca175451585962e1b
Ankit Tiwari <ank947214@gmail.com>
2024-07-30 18:54:31 +0000

    do lop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -19,9 +19,24 @@ for (let j= 0; j < 10; j++) {
 // break and continue 
 
 
-for (let index = 0; index < 15; index++) {
+// for (let index = 0; index < 12 ; index++) {
+//   if(index ===5){
+// console.log('detected 5');
+// break
+//   } 
+// console.log('value of i is ${index}');
+// }
 
-    
-console.log('value of i is ${index}');
 
-}
+
+// for (let index = 0; index < 10 ; index++) {
+//   if(index ===5){
+// console.log('detected 5');
+// continue ;
+//   } 
+// console.log('value of i is ${index}');
+// }
+
+
+
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,44 @@
+let index = 0
+while (index <=10){
+   // console.log('value of index is ${index}'); //print karne ka method
+    index = index +2
+}
+
+// let myarray = ['flash' , "batman" ,'spiderman']
+// let arr
+// = 0
+// while(arr < myarray.length) {
+//    console.log('value is ${myarray[arr]}');
+// }
+
+
+let score = 14
+do {
+     //console.log('score is ${score}');
+     score++
+}while (score<=10);
+
+const arr = [5,4,3,2,1]
+
+//yaha par bas declaration karna hota hain
+ for(const num of arr){
+    // console.log(num);
+ }
+
+//  const greetings= "Hello World"
+//  for(const grret of greetings{console.log('Each char is ${greet}')}
+
+// Maps -duplicate entries is not allowed all are unique
+
+
+const map = new Map()
+map.set('IN' ,"India")
+map.set('ch' ,"chinese")
+map.set('ban' ,"bangladesh")
+
+// console.log(map);
+
+for( const key of map )
+
+
+
